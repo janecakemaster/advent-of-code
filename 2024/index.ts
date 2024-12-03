2f53720697ad8d22a57eb4120d048ac5adb89d4b
jane kim <jane.kim@volleygames.com>
2024-12-02 17:55:17 -0800

    cleanup

--- 2024/index.ts
+++ 2024/index.ts
@@ -2,9 +2,6 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { day1 } from "./day1";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const functions = {
   day1,
 };
@@ -12,8 +9,7 @@ const functions = {
 const functionName = process.argv[2];
 
 if (functionName in functions) {
-  // Call the function with spread arguments
   functions[functionName]();
 } else {
-  console.log(`${functionName.replace("day", "day ")} not implemented!`);
+  console.log(`${functionName.replace("day", "day ")} not solved!`);
 }
