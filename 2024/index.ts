c1f8bde364d8636dabe7b46ac36e996972b74f83
jane kim <jane.kim@volleygames.com>
2024-12-02 17:19:18 -0800

    run script

--- 2024/index.ts
+++ 2024/index.ts
@@ -1,1 +1,15 @@
-console.log('hey')
+try {
+  const functions = {
+  };
+
+  const functionName = process.argv[2];
+
+  if (functionName in functions) {
+    // Call the function with spread arguments
+    functions[functionName]();
+  } else {
+    console.log(`${functionName.replace('day', 'day ')} not implemented!`);
+  }
+} catch (error) {
+  console.log('please provide a day to run (e.g. day1)');
+}
